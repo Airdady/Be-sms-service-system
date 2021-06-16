a32a21252d1db9afdd7520cd3f4e4127e67fd873
swaibu <sswaib11@gmail.com>
2021-06-16 13:58:16 +0300

    hot fix register

--- src/api/sms/sms.controller.js
+++ src/api/sms/sms.controller.js
@@ -9,7 +9,7 @@ const SMSController = {
 		const reqData = { from: from || senderId, to, content, dlr, dlrUrl };
 		try {
 			const { data } = await SmsRouter.post(`/send`, reqData);
-			await CreateLog('sms', userId, to, from, data.data.split(' ')[1].replace(/[/"]/,''));
+			await CreateLog('sms', userId, to, from, data.data.split(' ')[1].replace(/[/"]/, ''));
 			return Response(res, 200, 'SMS successfully sent', data);
 		} catch (error) {
 			return res.status(422).send(error);
