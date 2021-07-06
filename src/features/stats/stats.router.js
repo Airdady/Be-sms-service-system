c07e3abff7c8e58ffa40904c6b0a405196f30006
isiagi <isiagigeofrey0@gmail.com>
2021-07-06 11:38:45 +0300

    adds sms and verify filters

--- src/features/stats/stats.router.js
+++ src/features/stats/stats.router.js
@@ -9,8 +9,14 @@ const router = express.Router();
 router.get('/', (req, res) => {
 	Logs.find({ userId: req.user._id }, (error, data) => {
 		if (error) return Res(res, 422, 'error while retrieving stats');
-		return Res(res,200,'', _.groupBy(data, (b) => moment(b.createdAt).format('YYYY-MM-DD')));
+		const count = _.groupBy(data, (b) => moment(b.createdAt).format('YYYY-MM-DD'))
+		
+		return Res(res,200,'',Object.values(count).map((value) => {
+			const sms =  value.filter((data) => data.serviceType === 'sms');
+			const verify = value.filter((data) => data.serviceType === 'verify');
+			return {date: moment(value[0].createdAt).format("MMM Do YY"), sms:sms ? sms.length : 0, verify: verify ? verify.length : 0};
+		}));
 	});
 });
-
+ 
 export default router;
