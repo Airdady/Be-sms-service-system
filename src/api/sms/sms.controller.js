395ad2452949da2e5420501477bfbc073d489454
isiagi <isiagigeofrey0@gmail.com>
2021-07-09 09:53:43 +0300

    adds dlr-url key

--- src/api/sms/sms.controller.js
+++ src/api/sms/sms.controller.js
@@ -6,7 +6,7 @@ const SMSController = {
 	send: async (req, res) => {
 		const { senderId, userId, dlr, dlrUrl } = req.profile;
 		const { from, to, content } = req.body;
-		const reqData = { from: from || senderId, to, content, dlr, dlrUrl };
+		const reqData = { from: from || senderId, to, content, dlr, 'dlr-url':dlrUrl };
 		try {
 			const { data } = await SmsRouter.post(`/send`, reqData);
 			const smsId = data.data.split(' ')[1].replace('"', '');
